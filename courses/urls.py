18a92578d21579d1c8705722bd4c5dc463ebadeb
yesaroun <akor1@naver.com>
2023-03-07 18:48:54 +0900

    fix: url fix

--- courses/urls.py
+++ courses/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import CourseList, CourseDetail
 
 
-urlpatterns = [path("", CourseList.as_view()), path("<int:pk>", CourseDetail.as_view())]
+urlpatterns = [
+    path("", CourseList.as_view()),
+    path("<int:pk>", CourseDetail.as_view()),
+]

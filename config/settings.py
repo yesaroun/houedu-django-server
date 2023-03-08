25c07654db9985613954c08d953a5c12fa88a833
yesaroun <akor1@naver.com>
2023-03-08 18:41:13 +0900

    fix : allowed hosts 추가

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+APPROVED_HOSTS = ["172.31.57.152"]

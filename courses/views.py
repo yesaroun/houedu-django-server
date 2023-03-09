6236e8e9345d5dfe5d1cfbcc13daf4c01ddddb17
yesaroun <akor1@naver.com>
2023-03-09 11:13:59 +0900

    fix: generic api view 수정

--- courses/views.py
+++ courses/views.py
@@ -1,27 +1,15 @@
 from django.db.models import QuerySet
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.serializers import SerializerMetaclass
 from .models import Course
 from .serializers import CourseSerializer
 
 
-class CourseList(ListCreateAPIView):
+class CourseList(ListAPIView):
     queryset: QuerySet = Course.objects.all()
     serializer_class: SerializerMetaclass = CourseSerializer
 
-    def get_queryset(self) -> QuerySet:
-        return self.queryset
 
-    def get_serializer_class(self) -> SerializerMetaclass:
-        return self.serializer_class
-
-
-class CourseDetail(RetrieveUpdateDestroyAPIView):
+class CourseDetail(RetrieveAPIView):
     queryset: QuerySet = Course.objects.all()
     serializer_class: SerializerMetaclass = CourseSerializer
-
-    def get_object(self) -> Course:
-        return super().get_object()
-
-    def get_serializer(self) -> CourseSerializer:
-        return super().get_serializer()

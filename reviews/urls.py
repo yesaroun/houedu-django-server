d342ae2c21397fd431a8d174779c279742b85fae
yesaroun <akor1@naver.com>
2023-03-14 15:02:04 +0900

    feat: review detail api 생성

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns: list = [
     path("", views.Reviews.as_view()),
+    path("<int:pk>/", views.ReviewDetail.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,22 +1,56 @@
 from django.db.models import QuerySet
 from django.http import HttpResponse, HttpRequest
+from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Review
 from .serializers import ReviewSerializer
 from typing import List
 
 
 class Reviews(APIView):
-    def get(self: Review, request: HttpRequest) -> HttpResponse:
+
+    """전체 리뷰 리스트 apiview"""
+
+    def get(self, request: HttpRequest) -> HttpResponse:
         all_reviews: QuerySet[Review] = Review.objects.all()
         serializer: List[ReviewSerializer] = ReviewSerializer(all_reviews, many=True)
         return Response(serializer.data)
 
-    def post(self: Review, request: HttpRequest) -> HttpResponse:
-        serializer: List[ReviewSerializer] = ReviewSerializer(data=request.data)
+    def post(self, request: HttpRequest) -> HttpResponse:
+        serializer: ReviewSerializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
-            review = serializer.save()
+            review: Review = serializer.save()
             return Response(ReviewSerializer(review).data)
         else:
             return Response(serializer.errors)
+
+
+class ReviewDetail(APIView):
+
+    """상세 리뷰 apiview"""
+
+    def get_object(self, pk) -> Review:
+        try:
+            return Review.objects.get(pk=pk)
+        except Review.DoesNotExist:
+            raise NotFound
+
+    def get(self, request: HttpRequest, pk: int) -> HttpResponse:
+        review: Review = self.get_object(pk)
+        serializer: ReviewSerializer = ReviewSerializer(
+            review,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_review: Review = serializer.save()
+            return Response(ReviewSerializer(updated_review).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request: HttpRequest, pk: int) -> HttpResponse:
+        review: Review = self.get_object(pk)
+        review.delete()
+        return Response(status=HTTP_204_NO_CONTENT)

fa031a7c07f84e19a4f8e4344b4bcf6fed4a08a0
yesaroun <akor1@naver.com>
2023-03-22 18:09:05 +0900

    debug 세팅

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "192.168.56.101",

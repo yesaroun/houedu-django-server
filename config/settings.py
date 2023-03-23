618d69d9997b0e10200397467990d7d9aa4e7db9
yesaroun <akor1@naver.com>
2023-03-23 14:13:42 +0900

    debug setting

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # render
 # DEBUG = "RENDER" not in os.environ

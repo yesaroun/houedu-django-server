69bc4b2819b900f81497b09d2e90168f9c284d02
yesaroun <akor1@naver.com>
2023-03-28 14:02:01 +0900

    build

--- config/settings.py
+++ config/settings.py
@@ -103,11 +103,11 @@ ROOT_URLCONF = "config.urls"
 # 추가
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://houedu.duckdns.org"]
-CORS_ORIGIN_REGEX_WHITELIST = ["http://localhost:3000", "http://houedu.duckdns.org/"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_REGEX_WHITELIST = ["http://localhost:3000"]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://houedu.duckdns.org/"]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://houedu.duckdns.org/"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
 TEMPLATES = [
     {

1583995d70b3820172fe651c305bfed599e98773
yesaroun <akor1@naver.com>
2023-03-28 13:42:03 +0900

    build

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # render
 # DEBUG = "RENDER" not in os.environ
 
@@ -103,11 +103,11 @@ ROOT_URLCONF = "config.urls"
 # 추가
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
-CORS_ORIGIN_REGEX_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://houedu.duckdns.org"]
+CORS_ORIGIN_REGEX_WHITELIST = ["http://localhost:3000", "http://houedu.duckdns.org/"]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://houedu.duckdns.org/"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://houedu.duckdns.org/"]
 
 TEMPLATES = [
     {

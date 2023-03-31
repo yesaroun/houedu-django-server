39dee4713c17839aea23ad2a91ca9eb5c4255018
yesaroun <akor1@naver.com>
2023-04-01 01:03:00 +0900

    build

--- config/settings.py
+++ config/settings.py
@@ -208,7 +208,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # ROOT_DIR = os.path.dirname(BASE_DIR)
 #
